54ce3a44ce63d743c367dee06617f5fe9df00a53
pawlkris <paulkristapovich@gmail.com>
2017-12-05 15:57:26 -0500

    added backend calculation of reservation (v hacky, probably could add helper method to simplify)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,8 +14,18 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    startd = params[:reservation][:start_date].to_date.mjd #gives date in total number of days, so we can subtract end date from start date to get total number of days rented
+    endd = params[:reservation][:end_date].to_date.mjd
+    days_rented = (endd-startd).to_f
+    if days_rented < 1
+      days_rented = 1
+    end
+    item_daily_cost = Item.find(params[:reservation][:item_id]).cost_daily
+    params[:reservation][:total_cost] = days_rented * item_daily_cost
     @reservation = Reservation.new(reservation_params)
     if @reservation.valid?
+      @reservation.item.available = false
+      # currently, we have no switch to make reservation item status return to true.
       @reservation.save
       redirect_to reservation_path(@reservation.id)
     else
