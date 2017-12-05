05ad48738ef4d41b86aa9180be3880d5c838a479
George Gore <georgelgore@gmail.com>
2017-12-05 10:58:05 -0500

    add some validations to Review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :reservation
+
+  validates :reservation_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
 end
