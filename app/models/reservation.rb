191b04451ecee979213ff7fdf717af5957fa1375
George Gore <georgelgore@gmail.com>
2017-12-05 11:01:15 -0500

    add basic validations to Reservation class

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_many :reviews
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_cost, presence: true
+
 end
