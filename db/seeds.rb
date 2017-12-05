2b3ee6693d1ca0e650b9ce938b39a95e4f2b51e6
George Gore <georgelgore@gmail.com>
2017-12-04 20:07:01 -0500

    created and seeded seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.destroy_all
+User.destroy_all
+Item.destroy_all
+Reservation.destroy_all
+Review.destroy_all
+
+Location.create(name: "Manhattan")
+Location.create(name: "Brooklyn")
+Location.create(name: "Queens")
+Location.create(name: "Bronx")
+
+User.create(name: "George", email: "george@george.com", password: "george", location_id: 2)
+
+Item.create(name: '20 oz. Straight-Claw Rip Hammer', brand: 'Estwing', classification: 'hammer', cost_daily: 5.0, cost_hourly: 1.0, available: true, location_id: 2)
+
+Reservation.create(user_id: 1, item_id: 1, start_date: 'Mon, 04 Dec 2017 00:00:00 -0500', end_date:'Tues, 05 Dec 2017 00:00:00 -0500', total_cost: 5.0)
+
+Review.create(reservation_id: 1, content: "It worked!", rating: 7)
