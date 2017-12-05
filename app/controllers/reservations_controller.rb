43241af4add513b0ae5843726575da53bf0deeb3
pawlkris <paulkristapovich@gmail.com>
2017-12-05 17:24:41 -0500

    added total_cost_method to reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,14 +13,14 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    startd = params[:reservation][:start_date].to_date.mjd #gives date in total number of days, so we can subtract end date from start date to get total number of days rented
-    endd = params[:reservation][:end_date].to_date.mjd
-    days_rented = (endd-startd).to_f
-    if days_rented < 1
-      days_rented = 1
-    end
-    item_daily_cost = Item.find(params[:reservation][:item_id]).cost_daily
-    params[:reservation][:total_cost] = days_rented * item_daily_cost
+    # startd = params[:reservation][:start_date].to_date.mjd #gives date in total number of days, so we can subtract end date from start date to get total number of days rented
+    # endd = params[:reservation][:end_date].to_date.mjd
+    # days_rented = (endd-startd).to_f
+    # if days_rented < 1
+    #   days_rented = 1
+    # end
+    # item_daily_cost = Item.find(params[:reservation][:item_id]).cost_daily
+    params[:reservation][:total_cost] = calc_total_cost(params)
     @reservation = Reservation.new(reservation_params)
     if @reservation.valid?
       @reservation.item.available = false
@@ -45,6 +45,18 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:user_id, :item_id, :start_date, :end_date, :total_cost)
-
   end
+
+  def calc_total_cost(params)
+    startd = params[:reservation][:start_date].to_date.mjd #gives date in total number of days, so we can subtract end date from start date to get total number of days rented
+    endd = params[:reservation][:end_date].to_date.mjd
+    days_rented = (endd-startd).to_f
+    if days_rented < 1
+      days_rented = 1
+    end
+    item_daily_cost = Item.find(params[:reservation][:item_id]).cost_daily
+    days_rented * item_daily_cost
+  end
+
+  
 end
