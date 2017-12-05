e21ff5203c48025fbe65187f447a92f128b995fe
George Gore <georgelgore@gmail.com>
2017-12-05 10:59:50 -0500

    add basic validations to Item class

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_many :users, through: :reservations
   has_many :reviews, through: :reservations
   belongs_to :location
+
+  validates :name, presence: true
+  validates :brand, presence: true
+  validates :classification, presence: true
+  validates :cost_daily, presence: true
+  validates :cost_hourly, presence: true
+  validates :available, presence: true
+  validates :location_id, presence: true
+
 end
