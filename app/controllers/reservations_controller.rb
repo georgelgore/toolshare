ef134286435e556ce925d07228a0dd870d8513ee
dansbands <dansbands@hotmail.com>
2017-12-05 12:21:54 -0500

    building reservations controller and views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,35 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+    # byebug
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def new
+    @reservation = Reservation.new(reservation_params)
+  end
+
+  def create
+    # update with validation
+    @reservation = Reservation.create(reservation_params)
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+      # what method goes here?
+  end
+
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :item_id, :start_date, :end_date, :total_cost)
+
+  end
 end
