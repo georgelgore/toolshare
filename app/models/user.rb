74e1782fc0e7b736bb01ff36bab1203dd904d3d0
George Gore <georgelgore@gmail.com>
2017-12-05 09:15:18 -0500

    add basic validations to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :items, through: :reservations
   belongs_to :location
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :location_id, presence: true
+
 end
