4221b15b499db868f7b6cfb6a28c899dc87944c6
George Gore <georgelgore@gmail.com>
2017-12-06 16:14:17 -0500

    changed schema?

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20171206202608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "image_url", default: "profile/placeholder.png"
     t.index ["location_id"], name: "index_users_on_location_id"
   end
 
