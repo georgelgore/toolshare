76fe5bc1c65a50b875ecc4228023638e3f3ddf8f
dansbands <dansbands@hotmail.com>
2017-12-08 14:32:58 -0500

    fixed welcome search flash message

--- app/models/tool_type.rb
+++ app/models/tool_type.rb
@@ -2,4 +2,15 @@ class ToolType < ApplicationRecord
   has_many :items
 
   validates :name, presence: true
+
+
+  def self.is_a_tool_type?(tool_type)
+    if !ToolType.all.any?{|tool| tool.name == tool_type.capitalize}
+      # errors.add(:This, "tool isn't available. Please select from the list below.")
+      return false
+    else
+      return true
+    end
+  end
+
 end
